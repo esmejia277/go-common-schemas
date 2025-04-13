5b7e2be87c72b94e22b097f0212c236217041bf3
Esteban Mejia <estebanmejia277@gmail.com>
2025-04-13 02:30:52 -0500

    test

--- utils/data.go
+++ utils/data.go
@@ -5,3 +5,11 @@ import "fmt"
 func DestroySession() {
 	fmt.Println("Destroying session")
 }
+
+func OpenSession() {
+	fmt.Println("Opening session")
+}
+
+func PauseSession() {
+	fmt.Println("Pauising session")
+}
